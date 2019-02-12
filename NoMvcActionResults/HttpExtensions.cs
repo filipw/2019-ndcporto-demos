c08a32902488694a95189f0f5bfb17410e0d7dc0
filipw <wojcieszyn@gmail.com>
2019-02-12 17:23:55 +0100

    no mvc action results

---
+++ NoMvcActionResults/HttpExtensions.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoMvcActionResults
+{
+    public static class HttpExtensions
+    {
+        public static Task WriteActionResult<TResult>(this HttpResponse response, TResult result)
+            where TResult : IActionResult
+        {
+            var executor = response.HttpContext.RequestServices.GetService<IActionResultExecutor<TResult>>();
+
+            if (executor == null)
+            {
+                throw new InvalidOperationException($"No action result executor for {typeof(TResult).FullName} registered.");
+            }
+
+            var routeData = response.HttpContext.GetRouteData() ?? new RouteData();
+            var actionContext = new ActionContext(response.HttpContext, routeData, new ActionDescriptor());
+
+            return executor.ExecuteAsync(actionContext, result);
+        }
+
+        public static Task WriteObjectResult(this HttpResponse response, object result)
+        {
+            var executor = response.HttpContext.RequestServices.GetService<IActionResultExecutor<ObjectResult>>();
+
+            if (executor == null)
+            {
+                throw new InvalidOperationException($"No action result executor for {typeof(ObjectResult)} registered.");
+            }
+
+            var routeData = response.HttpContext.GetRouteData() ?? new RouteData();
+            var actionContext = new ActionContext(response.HttpContext, routeData, new ActionDescriptor());
+
+            return executor.ExecuteAsync(actionContext, new ObjectResult(result));
+        }
+
+        private static readonly JsonSerializer Serializer = new JsonSerializer();
+
+        public static void WriteJson<T>(this HttpResponse response, T obj)
+        {
+            response.ContentType = "application/json";
+            using (var writer = new HttpResponseStreamWriter(response.Body, Encoding.UTF8))
+            {
+                using (var jsonWriter = new JsonTextWriter(writer))
+                {
+                    jsonWriter.CloseOutput = false;
+                    jsonWriter.AutoCompleteOnClose = false;
+
+                    Serializer.Serialize(jsonWriter, obj);
+                }
+            }
+        }
+
+        public static T ReadFromJson<T>(this HttpContext httpContext)
+        {
+            using (var streamReader = new StreamReader(httpContext.Request.Body))
+            using (var jsonTextReader = new JsonTextReader(streamReader))
+            {
+                var obj = Serializer.Deserialize<T>(jsonTextReader);
+
+                var results = new List<ValidationResult>();
+                if (Validator.TryValidateObject(obj, new ValidationContext(obj), results))
+                {
+                    return obj;
+                }
+
+                httpContext.Response.StatusCode = 400;
+                httpContext.Response.WriteJson(results);
+
+                return default(T);
+            }
+        }
+    }
+}
---
+++ NoMvcActionResults/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using NoMvcActionResults.Data;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+
+namespace NoMvcActionResults
+{
+    public class Program
+    {
+        public static async Task Main(string[] args) =>
+            await WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(s =>
+                {
+                    s.AddEmbeddedIdentityServer();
+                    s.AddRouting();
+                    s.AddMvcCore().AddJsonFormatters();
+                })
+                .Configure(app =>
+                {
+                    app.Map("/identity", id =>
+                    {
+                        // use embedded identity server to issue tokens
+                        id.UseIdentityServer();
+                    })
+                    .UseAuthentication() // consume the JWT tokens in the API
+                    .Use(async (c, next) => // authorize the whole API against the API policy
+                    {
+                        var allowed = await c.RequestServices.GetRequiredService<IAuthorizationService>().AuthorizeAsync(c.User, "API");
+                        if (!allowed.Succeeded)
+                        {
+                            c.Response.StatusCode = 401;
+                            return;
+                        }
+
+                        await next();
+                    })
+                    .UseRouter(r => // define all API endpoints
+                    {
+                        var contactRepo = new InMemoryContactRepository();
+
+                        r.MapGet("contacts", async (request, response, routeData) =>
+                        {
+                            var contacts = await contactRepo.GetAll();
+                            await response.WriteObjectResult(contacts);
+                        });
+
+                        r.MapGet("contacts/{id:int}", async (request, response, routeData) =>
+                        {
+                            var contact = await contactRepo.Get(Convert.ToInt32(routeData.Values["id"]));
+                            if (contact == null)
+                            {
+                                response.StatusCode = 404;
+                                return;
+                            }
+
+                            await response.WriteObjectResult(contact);
+                        });
+
+                        r.MapPost("contacts", async (request, response, routeData) =>
+                        {
+                            var newContact = request.HttpContext.ReadFromJson<Contact>();
+                            if (newContact == null) return;
+
+                            await contactRepo.Add(newContact);
+
+                            response.StatusCode = 201;
+                            await response.WriteObjectResult(newContact);
+                        });
+
+                        r.MapPut("contacts/{id:int}", async (request, response, routeData) =>
+                        {
+                            var updatedContact = request.HttpContext.ReadFromJson<Contact>();
+                            if (updatedContact == null) return;
+
+                            updatedContact.ContactId = Convert.ToInt32(routeData.Values["id"]);
+                            await contactRepo.Update(updatedContact);
+
+                            response.StatusCode = 204;
+                            await response.WriteObjectResult(updatedContact);
+                        });
+
+                        r.MapDelete("contacts/{id:int}", async (request, response, routeData) =>
+                        {
+                            await contactRepo.Delete(Convert.ToInt32(routeData.Values["id"]));
+                            response.StatusCode = 204;
+                        });
+                    });
+                })
+                .Build().RunAsync();
+    }
+}
