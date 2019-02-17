4659a1d6b5e181c778dc67e1082c14d3077f18e1
filipw <wojcieszyn@gmail.com>
2019-02-17 13:18:55 +0100

    updated demo to webapicontrib.core

--- src/NoMvcActionResults/Program.cs
+++ src/NoMvcActionResults/Program.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
+using WebApiContrib.Core.Results;
 
 namespace NoMvcActionResults
 {
@@ -32,15 +33,44 @@ namespace NoMvcActionResults
                                 new Contact { Name = "Filip", City = "Zurich" },
                                 new Contact { Name = "No Filip", City = "Not Zurich" }
                             };
-                            await response.WriteActionResult(new ObjectResult(contacts));
+
+                            var objectResult = new ObjectResult(contacts);
+                            await response.WriteActionResult(objectResult);
                         });
 
 
                         r.MapGet("download", async (request, response, routeData) =>
                         {
-                            var file = Path.GetFullPath(Path.Combine("Files", "powershell.pdf"));
-                            await response.WriteActionResult(new PhysicalFileResult(file, "application/pdf"));
+                            var path = Path.GetFullPath(Path.Combine("Files", "powershell.pdf"));
+                            var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 
+                                64 * 1024, FileOptions.Asynchronous | FileOptions.SequentialScan);
+
+                            await response.WriteActionResult(new FileStreamResult(fileStream, "application/pdf"));
                         });
+
+                        #region simpler
+                        r.MapGet("simpler/contacts", async (request, response, routeData) =>
+                        {
+                            var contacts = new[]
+                            {
+                                new Contact { Name = "Filip", City = "Zurich" },
+                                new Contact { Name = "No Filip", City = "Not Zurich" }
+                            };
+
+                            // from WebApiContrib.Core
+                            await response.HttpContext.Ok(contacts);
+                        });
+
+
+                        r.MapGet("simpler/download", async (request, response, routeData) =>
+                        {
+                            var path = Path.GetFullPath(Path.Combine("Files", "powershell.pdf"));
+
+                            // from WebApiContrib.Core
+                            await response.HttpContext.PhysicalFile(path, "application/pdf");
+                        });
+
+                        #endregion
                     });
                 })
                 .Build().RunAsync();
